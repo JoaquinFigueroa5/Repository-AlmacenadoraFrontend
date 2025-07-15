4da7177c95b2381b6646ebf6c3ca28d893e08968
LuisDeLeon24 <168132018+LuisDeLeon24@users.noreply.github.com>
2025-07-14 21:27:48 -0600

    Conexion a backend desplegado

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:3000/almacenadora/v1',
+    baseURL: 'https://almacenadora-backend.onrender.com',
     timeout: 5000
 })
 
